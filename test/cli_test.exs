180b940669f0ff6ccd9c5ab45f7557624c46656f
Clint Kelly <clint.kelly@airbnb.com>
2016-06-22 14:11:05 -0700

    Add HTTPoison and more tests

--- test/cli_test.exs
+++ test/cli_test.exs
@@ -2,7 +2,11 @@ defmodule CliTest do
   use ExUnit.Case
   doctest Issues
 
-  import Issues.CLI, only: [parse_args: 1]
+  import Issues.CLI, only: [
+    parse_args: 1,
+    sort_into_ascending_order: 1,
+    convert_to_list_of_maps: 1
+  ]
 
   test ":help returned by option parsing with -h and --help options" do
     assert parse_args(~w(-h anything)) == :help
@@ -15,5 +19,18 @@ defmodule CliTest do
 
   test "count is default if two values given" do
     assert parse_args(~w(user project)) == {"user", "project", 4}
+  end
+
+  test "sort ascending orders the correct way" do
+    issues = ~w(c a b)
+    |> fake_create_at_list
+    |> sort_into_ascending_order
+    |> Enum.map(&(&1["created_at"]))
+    assert issues == ~w(a b c)
+  end
+
+  defp fake_create_at_list(values) do
+    data = for value <- values, do: [{"created_at", value}, {"other_data", "xxx"}]
+    convert_to_list_of_maps data
   end
 end
